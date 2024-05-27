b212f6f3384a9a674efd2c882b622d0d9e6c0ca7
Testing <test>
2024-05-27 16:39:29 +0800

    Update printScreen function in feature branch

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+void printScreen(vector<string> v){
+    for(auto s : v){
+        cout << s << endl;
+    }
+}
 
 int main(){
     cout << "Hello World" << endl;
-    cout << "Sousou no Frieren" << endl;
+    vector<string> map;
+    for (int i = 0; i < 10; i++){
+        map.push_back("##########");
+    }
+    printScreen(map);
     return 0;
 }
